98d7c2b3180416ed4b6d7be736fb0db28ba4df46
dragonwarrior9873 <dragonwarrior9873@gmail.com>
2024-06-26 06:00:27 +0900

    first commit

---
+++ packages/wallet-adapter-react/src/index.tsx
@@ -0,0 +1,5 @@
+export * from "@aptos-labs/wallet-adapter-core";
+export * from "./WalletProvider";
+export * from "./components/AptosPrivacyPolicy";
+export * from "./components/WalletItem";
+export * from "./useWallet";
